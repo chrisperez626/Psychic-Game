e432eb3d4d574a4943ceee7efe51de34a7ccd9bf
chrisperez626 <chrisperez626@gmail.com>
2018-03-15 20:40:06 -0400

    initial program files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Psychic-Game
+In this game simply click a letter on the keyboard to guess what the psychic which is the compueter
+is thinking.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,33 @@ var wins = 0;
 
 //Total losses
 var losses = 0;
-
+//Total Guesses
 var Guesses = 9;
+//array to setup letters guessed
 var guessesUsed = [];
-
+//function for game
 document.onkeyup = function(event){
+    //allows key that user presses to stand as guess
     var userGuess = event.key;
+    //lets the computer guess a random number
     var pyschicChoice = pyschicOptions[Math.floor(Math.random()*pyschicOptions.length)];
-    
+    //makes the wrong letters guessed list
     if(guessesUsed.indexOf(userGuess) < 0 && pyschicOptions.indexOf(userGuess) >= 0){
         guessesUsed[guessesUsed.length] = userGuess;
     };
+    // if user gets it right guesses restars, wins increase, 
+    // and guessed letters clears
     if(userGuess===pyschicChoice){
         wins++;
         Guesses = 9;
         guessesUsed = [];
     }
+    //if not correct guesses decrease
     else{
         Guesses--;
     }
-
+    //if user runs out of guesses losses decrease, losses increase,
+    //and guessed letters erase
     if(Guesses===0){
         losses++;
         Guesses=9;
@@ -34,14 +41,14 @@ document.onkeyup = function(event){
     
     
     
-
+    //variable that makes the elements writeable on a webpage
     var html = 
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" +
     "<p>Guesses: " + Guesses + "</p>" +
     "<p>Letters Guessed: " + guessesUsed + "</p>";
     
-
+    //writes the amount of all elements on the webpage
     document.querySelector("#game").innerHTML=html;
     
 
